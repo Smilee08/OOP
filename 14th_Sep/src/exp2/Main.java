c755f44281cdec8ca6f69af67ec75410abfce82a
Smilee08 <94034724+Smilee08@users.noreply.github.com>
2023-01-04 12:17:16 +0530

    Add files via upload

---
+++ 14th_Sep/src/exp2/Main.java
@@ -0,0 +1,23 @@
+package exp2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Student s1= new Student();
+		 Student s2= new Student();
+		 s1.rollno=2268;
+		 s1.name="Smilee";
+		 s1.vaddo="Saleria";
+		 s1.city="panjim";
+		 s1.country="India";
+		 
+		 s1.print();
+		 s1.Address();
+		 
+		 s2.rollno=2210;
+		 s2.name="Smita";
+		 s2.print();
+	}
+
+}
---
+++ 14th_Sep/src/exp2/Student.java
@@ -0,0 +1,26 @@
+package exp2;
+
+public class Student {
+	Integer rollno;
+	String name;
+	String vaddo;
+	String city;
+	String country;
+	
+	
+	 void print() {
+	System.out.println("Roll No:"+rollno);
+	System.out.println("Name:"+name);
+	System.out.println("\n");
+	}
+
+	 void Address() {
+		 System.out.println("Address:");
+		 System.out.println("Vaddo:"+vaddo);
+		 System.out.println("City:"+city);
+		 System.out.println("Country:"+country);
+			System.out.println("\n");
+		 
+	 }
+}
+
